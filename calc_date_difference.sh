35166a0699f8d503cd69abf96478f343da411d93
Jessica <jessica.keune@ugent.be>
2021-10-13 17:43:30 +0200

    style: date format; hours difference

--- calc_date_difference.sh
+++ calc_date_difference.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-TSini="20030101"
+TSini="2003-01-01"
 iday=10
-TSend=`date -d "$TSini + $iday day" +%Y%m%d`
+TSend=`date -d "$TSini + $iday day" +%Y-%m-%d`
 
 TI_sec=$((`date -d $TSend +%s` - `date -d $TSini +%s`))
 TI_hours=$(($TI_sec/3600))
+TI_days=$(($TI_sec/3600/24))
 echo "Start date:                $TSini"
 echo "End date:                  $TSend"
 echo "Time difference [seconds]: $TI_sec"
 echo "Time difference [hours]:   $TI_hours"
+echo "Time difference [days]:    $TI_days"
