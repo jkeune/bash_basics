d7dd81031bf177f6291c40de483de69f9f27ba2f
Jessica <jessica.keune@ugent.be>
2021-10-13 15:53:04 +0200

    feat: script to change the group permissions for a folder correctly

--- set_group_permissions.sh
+++ set_group_permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# usage: ./set_permissions.sh folder group
+# usage: ./set_group_permissions.sh folder group
 # changes the permissions of "folder" and all its files to the group "group"
 folder=$1
 group=$2
