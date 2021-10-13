e8889d63e694ae31e207319508f4acba7f499b1a
Jessica <jessica.keune@ugent.be>
2021-10-13 17:41:37 +0200

    feat: script to calculate date difference

---
+++ calc_date_difference.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TSini="20030101"
+iday=10
+TSend=`date -d "$TSini + $iday day" +%Y%m%d`
+
+TI_sec=$((`date -d $TSend +%s` - `date -d $TSini +%s`))
+TI_hours=$(($TI_sec/3600))
+echo "Start date:                $TSini"
+echo "End date:                  $TSend"
+echo "Time difference [seconds]: $TI_sec"
+echo "Time difference [hours]:   $TI_hours"
